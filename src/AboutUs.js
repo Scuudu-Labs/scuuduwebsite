b1f6ad3d46e6959f9fabad0ef41dccac77d37ba6
joshuanwankwo <systemblakes@gmail.com>
2021-01-29 14:16:42 +0100

    Done with About Us page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,22 +1,21 @@
 import React from "react";
-import NavBar from "./Components/NavBar/NavBar";
-// import logo from '../src/assets/text-logo.png'
-// import TopHeader from "./Components/TopHeader/TopHeader";
 import WhoWeAre from "./Components/WhoWeAre/WhoWeAre";
 import Subscribe from "./Components/Subscribe/Subcribe";
 import Footer from "./Components/Footer/Footer";
 import AboutUsBanner from "./Components/AboutUsBanner/AboutUsBanner";
 import OurStory from "./Components/OurStory/OurStory";
+import OurExpertise from "./Components/OurExpertise/OurExpertise";
+import Awards from "./Components/Awards/Awards";
 
 class AboutUs extends React.Component {
   render() {
     return (
       <div>
-        {/* <TopHeader /> */}
-        {/* <NavBar logo={logo}/> */}
         <AboutUsBanner />
         <WhoWeAre pad='0px 0px'/>
         <OurStory />
+        <OurExpertise />
+        <Awards />
         <Subscribe />
         <Footer />
       </div>
---
+++ src/Components/Awards/Awards.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Awards.css";
+import forbes from '../../assets/forbes.png'
+import fsiFinals from '../../assets/fsiFinals.png'
+
+class Awards extends React.Component {
+  render() {
+    return(
+      <div id='awards-container'>
+        <div id='awards-inner-container'>
+          <h1>Awards & Recognitions</h1>
+          <p>
+            We have been working on a variety of projects and across different
+            industries across disciplines, making our work an appropriate,
+            versatile and functional response to the unique needs of our
+            clients. Our capabilities cover the following areas;
+          </p>
+        <div id='award-image-con'>
+        <div id='award-image'>
+            <img src={forbes} />
+            <p>2020. Forbes Nigeria Summit Finalist</p>
+          </div>
+          <div id='award-image'>
+            <img src={fsiFinals}/>
+            <p>2020. FSI Hackathon 2nd Runner-up</p>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Awards;
