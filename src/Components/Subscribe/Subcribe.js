3f0114bb1a004d6da2f6b85ddfc1509f126e12c7
joshuanwankwo <systemblakes@gmail.com>
2021-02-19 14:36:09 +0100

    consumed Emeka's API like as if it is spaghetti and fried ripe plantain

--- src/Components/Subscribe/Subcribe.js
+++ src/Components/Subscribe/Subcribe.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Subscribe.css";
 
-class Subscribe extends React.Component {
-  render() {
+
+const Subscribe = () => {
+    // const [todos, setTodos] = useState(null);
+    const [value, setValue] = useState('');
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      const data = { email_address: value };
+      console.log('submit');
+      console.log(value);
+      fetch('https://scuudu-apis.herokuapp.com/astroworld/new-subscriber?APIKey=zdakcsdenkwrWQER12vevlewewfwrfwkfefe23134', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then(res => res.json())
+        .then(res => console.log(res));
+    }
+  
+    function handleValue(e) {
+      setValue(e.target.value);
+    }
     return (
       <div id="subscribe-container">
         <div id="subscribe-bold-text">
@@ -14,14 +36,13 @@ class Subscribe extends React.Component {
             who are first to get the latest updates about our intriguing
             activities.
           </p>
-          <form>
-              <input id='email' type='text' placeholder='Email' />
+          <form action="" onSubmit={handleSubmit}>
+              <input id='email' type='text' placeholder='Email' onChange={handleValue} />
               <input id='submit' type='submit' value='Subscribe' />
           </form>
         </div>
       </div>
     );
-  }
 }
 
 export default Subscribe;
