876eee63d55596c41e1ec85b080f6dc4ed642ad5
ScuuduWorldwide <64476451+ScuuduWorldwide@users.noreply.github.com>
2021-04-12 16:26:43 +0100

    Revert "Add test"

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import React from "react";
-import AboutUs from "./AboutUs";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
 
-import HomePage from "./HomePage";
-import ServicesPage from "./ServicesPage";
-import Products from "./Product";
-import MeetTheTeam from "./MeetTheTeam";
-import PageNotFOund from "./pageNotFound";
-import Store from "./Components/Store/Store";
 
-// const DefaultRoutes = () => {
-//   return (
-//     <Layout>
-//       <Switch>
-//         <Route path="/" exact component={HomePage} />
-//         <Route path="/ServicePage" component={ServicesPage} />
-//         <Route path="/AboutUs" component={AboutUs} />
-//         <Route path="/Products" component={Products} />
-//         <Route path="/MeetTheTeam" component={MeetTheTeam} />
-//         <Route path="*" component={ExternalRoutes} />
-//       </Switch>
-//     </Layout>
-//   );
-// };
-
-// const ExternalRoutes = () => {
-//   return (
-//     <Switch>
-//       <Route path="/Store" component={ComingSoon} />
-//       <Route path="*" component={PageNotFOund} />
-//     </Switch>
-//   );
-// };
-
-class App extends React.Component {
-  render() {
-    return (
-      <div id="app-container">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/Services" component={ServicesPage} />
-            <Route path="/AboutUs" component={AboutUs} />
-            <Route path="/Products" component={Products} />
-            <Route path="/MeetTheTeam" component={MeetTheTeam} />
-            <Route path="/Store" component={Store} />
-            <Route component={PageNotFOund} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+class App extends React.Component{
+    render(){
+        return(
+            <h1>Scuudu Website</h1>
+        )
+    }
 }
 
 export default App;
