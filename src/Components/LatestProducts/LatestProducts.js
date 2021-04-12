6a157922e3a6c445c53b8ad4f165f9f1f7841bfe
DevNonye <abonyi.chinonye.g20@gmail.com>
2021-04-12 10:00:58 +0100

    Update
    Update

--- src/Components/LatestProducts/LatestProducts.js
+++ src/Components/LatestProducts/LatestProducts.js
@@ -1,10 +1,10 @@
 import React from "react";
 import './LatestProducts.css'
 import ProductCard from "../ProductsCard/ProductCard";
-import greenCard from '../../assets/green-rec.png';
-import ashCard from '../../assets/ash-rec.png';
-import yelloward from '../../assets/yellow-rec.png';
-import tickAsh from '../../assets/tick-ash-rec.png';
+import Friday from '../../assets/Friday.png';
+import Wyrewolf from '../../assets/Wyrewolf.png';
+import Origin from '../../assets/Origins.png';
+import BuildAR from '../../assets/BuildAR.png';
 
 class LatestProducts extends React.Component {
   render() {
@@ -21,10 +21,10 @@ class LatestProducts extends React.Component {
         Take a look at some of our amazing products.
         </p>
         <div id='home-cards-container'>
-        <ProductCard homeImage={greenCard} homeName='Friday' homeDescription='A short description about the project 1' />
-        <ProductCard homeImage={ashCard} homeName='Wyrewolf' homeDescription='A short description about the project 1' />
-        <ProductCard homeImage={yelloward} homeName='Origin - Tales of Eri' homeDescription='A short description about the project 1' />
-        <ProductCard homeImage={tickAsh} homeName='BuildAR' homeDescription='A short description about the project 1' />
+        <ProductCard homeImage={Friday} homeName='Friday' homeDescription='A short description about the project 1' />
+        <ProductCard homeImage={Wyrewolf} homeName='Wyrewolf' homeDescription='A short description about the project 1' />
+        <ProductCard homeImage={Origin} homeName='Origin - Tales of Eri' homeDescription='A short description about the project 1' />
+        <ProductCard homeImage={BuildAR} homeName='BuildAR' homeDescription='A short description about the project 1' />
         </div>
         </div>
       </div>
